74e6b5b6c73f6ee86d596785efe110596d879816
user <user@DESKTOP-2JQTQPV>
2022-03-16 01:37:44 +0400

    Add project files.

---
+++ GenericNet5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericNet5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] numbers = new int[] { 2, 3, 4, 5, 6, 23 };
+            string[] names = new string[] { "Gocha","Gio", "Levani"};
+
+            Writer(numbers);
+            Writer(names);
+
+        }
+
+        public static void Writer<T>(IEnumerable<T> ar)
+        {
+            foreach (var item in ar)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ GenericNet5/Services.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericNet5
+{
+    internal class Services
+    {
+        public void Writer<T>(List<T> ar)
+        {
+            foreach (var item in ar)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
